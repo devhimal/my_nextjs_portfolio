1f79ee62cc1ccbb1bd5b5cd2b5a7fc7370c83bb4
devhimal <himalhobbs.364@gmail.com>
2025-01-06 16:31:27 +0545

    feat: updated the title of about page in my portfolio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react"
 import Layout from "@/layout/Layout"
-import Me from '@/components/body/Me'
-import About from '@/components/body/About'
-import Hackcorruption from '@/components/body/Hackcorruption'
-import Skills from '@/components/body/Skills'
+import Me from "@/components/body/Me"
+import About from "@/components/body/About"
+import Hackcorruption from "@/components/body/Hackcorruption"
+import Skills from "@/components/body/Skills"
 import Projects from "@/components/body/Projects"
 
 const index = () => {
   return (
     <>
-    <Layout>
-      {/* <Me /> */}
-      <About />
-      <Projects />
-      <Hackcorruption />
-      <Skills />
-    </Layout>
+      <Layout>
+        {/* <Me /> */}
+        <About />
+        <Projects />
+        <Hackcorruption />
+        <Skills />
+      </Layout>
     </>
   )
 }
