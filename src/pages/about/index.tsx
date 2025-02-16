8ab5d6b3c9a66c4993e193c56fbb29fd54a63436
devhimal <himalhobbs.364@gmail.com>
2025-02-16 12:02:57 +0545

    feat: added contact us and about page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="h-[1000px] px-8 text-center gap-8 flex flex-col justify-center items-center ">
+      <h1>
+        Hi there, I am himal tamang, a BSIT student at kings college nepal.
+      </h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="h-[1000px] flex flex-col gap-8 items-center justify-center text-center capitalize">
+      <h1>Hello there, you can contact me through this number</h1>
+      <p>9800002922</p>
+    </div>
+  );
+};
+
+export default Contact;
